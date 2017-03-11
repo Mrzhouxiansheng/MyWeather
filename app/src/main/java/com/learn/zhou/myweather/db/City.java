9eba611b636a428f0e2dbea8ab0489fbd887390a
zhouli <2629062507@qq.com>
2017-03-11 11:05:55 +0800

    创建数据库和表

---
+++ app/src/main/java/com/learn/zhou/myweather/db/City.java
@@ -0,0 +1,10 @@
+package com.learn.zhou.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class City extends DataSupport {
+}
---
+++ app/src/main/java/com/learn/zhou/myweather/db/Province.java
@@ -0,0 +1,10 @@
+package com.learn.zhou.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class Province extends DataSupport {
+}
