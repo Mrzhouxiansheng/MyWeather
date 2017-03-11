26b024af6c7f09e3b1b3cf65811446c3bb712b43
zhouli <2629062507@qq.com>
2017-03-11 11:06:30 +0800

    创建数据库和表——补充提交

--- app/src/main/java/com/learn/zhou/myweather/db/City.java
+++ app/src/main/java/com/learn/zhou/myweather/db/City.java
@@ -2,9 +2,34 @@ package com.learn.zhou.myweather.db;
 
 import org.litepal.crud.DataSupport;
 
-/**
- * Created by Administrator on 2017/3/11.
- */
+//创建市级的实体类
 
 public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
 }
--- app/src/main/java/com/learn/zhou/myweather/db/Conty.java
+++ app/src/main/java/com/learn/zhou/myweather/db/Conty.java
@@ -2,9 +2,43 @@ package com.learn.zhou.myweather.db;
 
 import org.litepal.crud.DataSupport;
 
-/**
- * Created by Administrator on 2017/3/11.
- */
+//创建县的实体类及天气id
 
 public class Conty extends DataSupport {
+    private int id;
+    private String contyName;
+    private int contyCode;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getContyCode() {
+        return contyCode;
+    }
+
+    public void setContyCode(int contyCode) {
+        this.contyCode = contyCode;
+    }
+
+    public String getContyName() {
+        return contyName;
+    }
+
+    public void setContyName(String contyName) {
+        this.contyName = contyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
 }
